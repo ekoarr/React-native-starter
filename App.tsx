0e54f627c27a5d20c310a4543e1ab53ab6ffcaa4
ekoarr <kotakwah@gmail.com>
2021-06-14 21:23:01 +0800

    add splash screen    used packages:  expo-splash-screen, expo-app-loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import Navigation from './src/navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import { Entypo } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 const App = () => {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  async function prepare() {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+      await Font.loadAsync(Entypo.font);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setAppIsReady(true);
+    }
+  }
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return <AppLoading
+      startAsync={prepare}
+      onFinish={onLayoutRootView}
+      onError={console.warn}
+    />
+  }
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
